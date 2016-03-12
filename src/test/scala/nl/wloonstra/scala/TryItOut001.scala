9cee5dd4dba5cf320364da963604dc995fd6c953
Wiggert <wiggertloonstra@gmail.com>
2016-03-12 17:08:57 +0100

    new first try it out

---
+++ src/test/scala/nl/wloonstra/scala/TryItOut001.scala
@@ -0,0 +1,28 @@
+package nl.wloonstra.scala
+
+import org.scalatest.FunSuite
+
+class TryItOut001 extends FunSuite {
+
+  test("Play with some val's and var's. See what's possible and what not.") {
+    val scala = "fun"
+    // and more:
+
+  }
+
+  test("Square numbers") {
+    // 1) Define a function which calculates the square of a number
+    // 2) Try it out for some numbers and print the result
+
+
+  }
+
+  test("Factorial numbers. Example: 4! = 4 x 3 x 2 x 1 = 24") {
+    // 1) Define a function which calculates the factorial of a given number
+    // 2) Try it out for some numbers and print the result
+
+
+  }
+
+
+}
