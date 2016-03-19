0c4cb93f24e62913df5aa1ec58b66f00abf36552
Wiggert <wiggertloonstra@gmail.com>
2016-03-19 12:33:29 +0100

    added try it yourself 007 with extra features

---
+++ src/test/scala/nl/wloonstra/scala/TryItYourself007.scala
@@ -0,0 +1,27 @@
+package nl.wloonstra.scala
+
+import org.scalatest.FunSuite
+
+class TryItYourself007 extends FunSuite {
+
+  test("Book library application") {
+    // Your local book library asked you to automate the process of handing out and taking back their books.
+    // You are going to write a booklibrary application.
+    // Use the scala language features you have learned so far.
+
+    // Requirements:
+    // - Library consists of a collection of books. For inspiration see photo, or use your own books.
+    // - The library has members, called 'readers'.
+    // - A book is either on the shelf at the library or with a reader
+    // - When a book is not on the shelf, there can be a waiting list of multiple readers
+    // - When a reader returns the book and there are other readers waiting, the book is automatically assigned to the
+    //   reader waiting the longest.
+    // - A book can be at a reader for 3 weeks maximum.
+    // - The library doesn't like readers who return books too late. No reminder emails are send,
+    //   but when the reader returns the book too late, he will be removed from the member list of the bookstore.
+    //   (Hint: he should also be removed from waiting lists if necessary).
+
+
+  }
+
+}
